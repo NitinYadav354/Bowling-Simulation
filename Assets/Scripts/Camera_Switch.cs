3fea01d9270847f16bd5a3dde9e642f602ed2aec
Nitin Yadav <nyadav4625@gmail.com>
2025-07-15 12:18:24 +0530

    Implement basic ball delivery system using Rigidbody physics

--- Assets/Scripts/Camera_Switch.cs
+++ Assets/Scripts/Camera_Switch.cs
@@ -4,11 +4,14 @@ public class Camera_Switch : MonoBehaviour
 {
     public Camera mainCamera;
     public Camera secondaryCamera;
+
+    public Camera Bat_Cam;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         mainCamera.enabled = true;
         secondaryCamera.enabled = false;
+        Bat_Cam.enabled = false;
     }
 
     // Update is called once per frame
@@ -18,6 +21,12 @@ public class Camera_Switch : MonoBehaviour
         {
             mainCamera.enabled = !mainCamera.enabled;
             secondaryCamera.enabled = !secondaryCamera.enabled;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Bat_Cam.enabled = !Bat_Cam.enabled;
+            mainCamera.enabled = !mainCamera.enabled;
+            secondaryCamera.enabled = false;
         }
         
     }
--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -4,6 +4,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     private Rigidbody rb;
     public Vector3 initialVelocity = new Vector3(0f, 0f, 5f);
+    public Vector3 initialAngularVelocity = new Vector3(0f, 0f, 0f);
     public Vector3 initialPosition = new Vector3(-0.5f, 2.5f, -8f);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,12 +24,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision other) {
+    private void OnCollisionEnter(Collision other)
+    {
+
         if (other.gameObject.CompareTag("Pitch"))
         {
             Vector3 collisionPosition = other.contacts[0].point;
-            Debug.Log("pitched at: " + collisionPosition);
-            
+            Debug.Log("pitched at: " + (10.06f - collisionPosition.z));
+
         }
 
 
@@ -36,9 +39,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger: " + rb.position.y);
- 
-        
+        Debug.Log("Batsman: y=" + (rb.position.y -0.5f));
+
+    }
+
+        private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Stumps: y=" + (rb.position.y - 0.5f));
+
     }
 
     // Update is called once per frame
@@ -51,6 +59,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
             {
                 rb.position = initialPosition;
                 rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = initialAngularVelocity;
+                rb.rotation = Quaternion.Euler(0f, -90f, 0f);
                 rb.useGravity = false;
             }
 
